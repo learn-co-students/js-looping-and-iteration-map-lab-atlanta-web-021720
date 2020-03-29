1f84562a66a361a76b8830738109e1f680f9061b
laarchambault <laarchambault@gmail.com>
2020-03-29 16:10:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file.
+
+function lowerCaseDrivers(arr) {
+    let new_arr = arr.map(name => name.toLowerCase());
+    return new_arr;
+}
+
+function createObjectPairs(string) {
+    const nameArr = string.split(" ");
+    const pair = Object.assign({}, {firstName: nameArr[0]}, {lastName: nameArr[1]});
+    return pair;
+}
+
+function nameToAttributes(arr) {
+    let new_arr = arr.map(name => createObjectPairs(name));
+    return new_arr;
+}
+
+function attributesToPhrase(arr) {
+    let new_arr = arr.map(person => `${person.name} is from ${person.hometown}`);
+    return new_arr;
+}
+
